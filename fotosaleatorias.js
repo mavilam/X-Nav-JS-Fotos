0bedb24a4f6b0d669b451a6ac23a0bf2690f9a94
Juan Garc´ıa Garc´ıa <juan.garcia@micorreo.com>
2015-02-10 18:16:43 +0100

    Ejercicio de fotos aleatorias

---
+++ fotosaleatorias.js
@@ -0,0 +1,12 @@
+
+
+function randomImg(){
+	var index = Math.floor(Math.random() * 6);
+	var img = document.getElementById("imagenes");
+	var list = document.getElementById("e"+index);
+	
+	img.innerHTML = '<img src="'+ list.innerHTML + '"/>'
+}
+
+randomImg();
+//greasyforkS
